8b2aa37bf8e80294f72aa934c0e2fa8c99fe7091
KarishmaDamania <66870203+KarishmaDamania@users.noreply.github.com>
2021-07-20 04:38:31 +0400

    Final Commit

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -9,7 +9,7 @@ function CreateArea(props) {
   function handleChange(event) {
     const { name, value } = event.target;
 
-    setNote(prevNote => {
+    setNote((prevNote) => {
       return {
         ...prevNote,
         [name]: value
